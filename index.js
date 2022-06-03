99004052aec407a00d4f61f4d3612c9098f7d726
MuhammadWKT github <muhammadwajahat992@gmail.com>
2022-06-04 00:36:41 +0500

    ThirdRepo

---
+++ index.js
@@ -0,0 +1,26 @@
+let a = "My name is Wajahat Khan";
+
+console.log(a.substring(0, 11));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let name = "Wajahat"; 
+
+// let sername = "Khan";
+
+// console.log(" Hey My name Is " +name + " " +sername);
+// console.log(`Hey My name Is ${name} ${sername}!`)
+// const myStatement = `Hey My name Is ${name} ${sername}!` 
+
+// console.log(`My statement ${myStatement}`);
+
